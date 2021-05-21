de71e03263443ac74e0c7251a978e6af52003af1
Chianing <chianing9527@gmail.com>
2021-05-22 01:12:04 +0800

    offer 12

---
+++ src/main/java/com/chianing/leeCode/offer/Offer_12.java
@@ -0,0 +1,74 @@
+package com.chianing.leeCode.offer;
+
+/*
+矩阵中的路径
+
+给定一个m x n 二维字符网格board 和一个字符串单词word。如果word 存在于网格中，返回 true ；否则，返回 false 。
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+同一个单元格内的字母不允许被重复使用。
+
+示例 1：
+输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+输出：true
+示例 2：
+输入：board = [["a","b"],["c","d"]], word = "abcd"
+输出：false
+
+提示：
+1 <= board.length <= 200
+1 <= board[i].length <= 200
+board 和 word 仅由大小写英文字母组成
+*/
+public class Offer_12 {
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        String word = "ABCCED";
+
+        System.out.printf("result is %s%n", exist(board, word));
+    }
+
+    private static char[] words;
+    private static char[][] boardCopy;
+
+    public static boolean exist(char[][] board, String word) {
+        if (board.length == 0 || board[0].length == 0 || word == null || "".equals(word)) {
+            return false;
+        }
+        boardCopy = board;
+        words = word.toCharArray();
+
+        for (int row = 0; row < boardCopy.length; row++) {
+            for (int column = 0; column < boardCopy[0].length; column++) {
+                if (dfs(row, column, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean dfs(int row, int column, int wordIndex) {
+        if (row < 0 || column < 0 || row >= boardCopy.length ||
+                column >= boardCopy[0].length || boardCopy[row][column] != words[wordIndex]) {
+            return false;
+        }
+        if (wordIndex == words.length - 1) {
+            return true;
+        }
+
+        // 标记该节点已经访问过
+        boardCopy[row][column] = '/';
+        // 按右、下、左、上的顺序 判断节点是否在字符串中
+        boolean result = dfs(row, column + 1, wordIndex + 1) ||
+                dfs(row + 1, column, wordIndex + 1) ||
+                dfs(row, column - 1, wordIndex + 1) ||
+                dfs(row - 1, column, wordIndex + 1);
+        // 还原该节点
+        boardCopy[row][column] = words[wordIndex];
+
+        return result;
+    }
+
+}
--- src/main/java/com/chianing/leeCode/offer/Offer_18.java
+++ src/main/java/com/chianing/leeCode/offer/Offer_18.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.chianing.leeCode.structure.ListNode;
 
 /*
+删除链表的节点
+
 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
 返回删除后的链表的头节点。
 注意：此题对比原题有改动
@@ -20,7 +22,6 @@ import com.chianing.leeCode.structure.ListNode;
 
 说明：
 题目保证链表中节点的值互不相同
-
  */
 public class Offer_18 {
 
